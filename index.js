60fcbfef474dc8cc0b009c521f03ee6208d3b54e
Rolando Santamaria Maso <kyberneees@gmail.com>
2016-12-20 12:07:15 +0100

    Source code re-factorization.

--- index.js
+++ index.js
@@ -16,66 +16,10 @@
 
 const HttpClient = require('enapso-client-js').HttpClient;
 const EventEmitter = require('eventemitter2').EventEmitter2;
+const parse = require('./lib/parser.js');
 
-const parse = (client, service, response) => {
-    let api = response.API;
-    // iterating by service controllers
-    for (let cname in api) {
-        // controller object
-        let controller = {};
-        // getting controller API
-        let capi = api[cname];
-        // setting controller description
-        controller.description = capi.description;
-        controller.name = cname;
-        // generating controller methods
-        for (let index in capi.methods) {
-            let method = capi.methods[index].name;
-            let length = capi.methods[index].length;
-
-            if (!capi.methods[index].portable) {
-                (function (cname, method, length) {
-                    controller[method] = function () {
-                        return client.callMethod(service.name, cname, method,
-                            Array.prototype.slice.call(arguments, 0, length));
-                    };
-                })(cname, method, length);
-            } else {
-                (function (mapi) {
-                    controller[mapi.name] = function () {
-                        let args = arguments;
-                        return new Promise((resolve, reject) => {
-                            return new Function("return {f: " + mapi.sourceCode + "}")
-                                ()["f"].call(mapi.scope, {
-                                    data: arguments[0],
-                                    reply: function (msg) {
-                                        msg.code = 0;
-                                        resolve(msg);
-                                    },
-                                    fail: function (msg) {
-                                        msg.code = -1;
-                                        reject(msg);
-                                    }
-                                });
-                        });
-                    }
-                })(capi.methods[index]);
-            }
-        }
-        // upgrading the service object
-        service[cname] = controller;
-    }
-
-    return service;
-};
-
-class MicroServiceGenerator extends EventEmitter {
+class MicroServiceGenerator {
     constructor(config) {
-        super({
-            wildcard: true,
-            newListener: true
-        });
-
         this.config = config;
         this.client = new HttpClient({
             url: config.url || 'https://dash.innotrade.com/http',
@@ -88,8 +32,8 @@ class MicroServiceGenerator extends EventEmitter {
     open() {
         let self = this;
         return new Promise((resolve, reject) => {
-            self.getClient().open().then(() => {
-                return self.getClient().login();
+            self.client.open().then(() => {
+                return self.client.login();
             }).then(() => {
                 resolve();
             }).catch((err) => {
@@ -131,108 +75,10 @@ class MicroServiceGenerator extends EventEmitter {
                 reject(err);
             });
         });
-
-        /*
-        client.send({
-
-        }, {
-            OnSuccess: function (aResponse) {
-                var lAPI = aResponse.API;
-                // iterating by app controllers
-                for (var lCName in lAPI) {
-                    // controller object
-                    var lC = {};
-                    // getting controller API
-                    var lCAPI = lAPI[lCName];
-                    // setting controller description
-                    lC.description = lCAPI.description;
-                    lC.name = lCName;
-                    // generating controller methods
-                    for (var lIndex in lCAPI.methods) {
-                        var lMethod = lCAPI.methods[lIndex].name;
-                        var lLength = lCAPI.methods[lIndex].length;
-
-                        if (!lCAPI.methods[lIndex].portable) {
-                            (function (aCName, aMethod, aLength) {
-                                lC[aMethod] = function () {
-                                    lWSC.callScriptAppMethod(aApp, aCName, aMethod,
-                                        Array.prototype.slice.call(arguments, 0, aLength),
-                                        arguments[aLength]);
-                                };
-                            })(lCName, lMethod, lLength);
-                        } else {
-                            (function (aMethodAPI) {
-                                lC[aMethodAPI.name] = function () {
-                                    var lArgs = arguments;
-                                    return new Function("return {f: " + aMethodAPI.sourceCode + "}")
-                                        ()["f"].call(aMethodAPI.scope, {
-                                            data: arguments[0],
-                                            reply: function (aMsg) {
-                                                aMsg.code = 0;
-
-                                                if ("function" === typeof lArgs[1]) {
-                                                    lArgs[1](aMsg);
-                                                } else {
-                                                    if (lArgs[1]["OnSuccess"]) {
-                                                        lArgs[1]["OnSuccess"](aMsg);
-                                                    }
-                                                    if (lArgs[1]["OnResponse"]) {
-                                                        lArgs[1]["OnResponse"](aMsg);
-                                                    }
-                                                }
-                                            },
-                                            fail: function (aMsg) {
-                                                aMsg.code = -1;
-
-                                                if ("function" === typeof lArgs[1]) {
-                                                    lArgs[1](aMsg);
-                                                } else {
-                                                    if (lArgs[1]["OnFailure"]) {
-                                                        lArgs[1]["OnFailure"](aMsg);
-                                                    }
-                                                    if (lArgs[1]["OnResponse"]) {
-                                                        lArgs[1]["OnResponse"](aMsg);
-                                                    }
-                                                }
-                                            }
-                                        });
-                                };
-                            })(lCAPI.methods[lIndex]);
-                        }
-                    }
-                    // setting controller in app object
-                    service[lCName] = lC;
-                }
-
-                lWSC.addPlugIn({
-                    processToken: function (aToken) {
-                        if (aApp === aToken.ns && "event" === aToken.type) {
-                            if ("function" === typeof service[aToken.data.name]) {
-                                service[aToken.data.name](aToken.data);
-                            }
-                        }
-                    }
-                });
-
-                // calling success callback
-                aSuccessFn(service);
-            },
-            OnFailure: function (aToken) {
-                aFailureFn(aToken);
-            }
-        });
-
-        return service;
-    }
-    */
     }
 
     close() {
         return this.client.close();
-    }
-
-    getClient() {
-        return this.client;
     }
 }
 
